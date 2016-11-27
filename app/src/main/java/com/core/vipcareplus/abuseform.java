0b17da47ad87b76321d82dbff5db0733b6763a09
Rudra Gupta <grudra7714@gmail.com>
2016-11-27 14:43:05 -0500

    changes

--- app/src/main/java/com/core/vipcareplus/abuseform.java
+++ app/src/main/java/com/core/vipcareplus/abuseform.java
@@ -2,12 +2,54 @@ package com.core.vipcareplus;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
-public class abuseform extends AppCompatActivity {
+import static android.widget.Toast.LENGTH_SHORT;
+
+public class abuseform extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_abuseform2);
+        Button save = (Button)findViewById(R.id.save_abuse_form);
+        LinearLayout record = (LinearLayout)findViewById(R.id.record_ll);
+        record.setOnClickListener(this);
+
+        save.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.save_abuse_form:
+//                Toast.makeText(getApplicationContext(), "SAVING...", Toast.LENGTH_LONG).show();
+//                Toast.makeText(getApplicationContext(), "SAVED", Toast.LENGTH_LONG).show();
+                LayoutInflater inflater = getLayoutInflater();
+                View toastLayout = inflater.inflate(R.layout.custom_toast, (ViewGroup) findViewById(R.id.custom_toast_layout));
+
+                Toast toast = new Toast(getApplicationContext());
+                toast.setDuration(Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.CENTER_HORIZONTAL, 0, 0);
+                toast.setView(toastLayout);
+                toast.show();
+                break;
+            case R.id.record_ll:
+                LayoutInflater inflater1 = getLayoutInflater();
+                View toastLayout1= inflater1.inflate(R.layout.custom_record_toast, (ViewGroup) findViewById(R.id.custom_toast_layout_record));
+
+                Toast toast1 = new Toast(getApplicationContext());
+                toast1.setDuration(Toast.LENGTH_LONG);
+                toast1.setGravity(Gravity.CENTER_HORIZONTAL, 0, 0);
+                toast1.setView(toastLayout1);
+                toast1.show();
+                break;
+        }
     }
 }
--- app/src/main/java/com/core/vipcareplus/abuselog.java
+++ app/src/main/java/com/core/vipcareplus/abuselog.java
@@ -18,16 +18,6 @@ public class abuselog extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_abuselog);
 
-        SQLiteDatabase db = openOrCreateDatabase("elderabusedb",MODE_PRIVATE,null);
-        db.execSQL("CREATE TABLE IF NOT EXISTS logs(attacktype TEXT,date VARCHAR, time VARCHAR, frequency VARCHAR, location VARCHAR, " +
-                "evidence VARCHAR, description VARCHAR );");
-
-        LinearLayout abuse_ll = (LinearLayout)findViewById(R.id.abuse_log_ll);
-        LinearLayout card_ll = (LinearLayout)findViewById(R.id.abuse_card_ll);
-
-        TextView type = (TextView)findViewById(R.id.abuse_form_assualt_text);
-        card_ll.addView(type);
-        abuse_ll.addView(card_ll);
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
