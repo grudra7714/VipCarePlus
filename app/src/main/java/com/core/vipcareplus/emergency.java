48e043329c637bc9b3d702a0231c0d6e61fdfc3d
deepak <deepakbegrajka@gmail.com>
2016-11-27 02:26:25 -0500

    Test commit

--- app/src/main/java/com/core/vipcareplus/emergency.java
+++ app/src/main/java/com/core/vipcareplus/emergency.java
@@ -7,6 +7,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 
 public class emergency extends AppCompatActivity {
 
@@ -17,7 +19,9 @@ public class emergency extends AppCompatActivity {
     }
 
     public void ringAlarm(View v){
-
+        Animation animation = new AlphaAnimation(1.0f, 0.0f);
+        animation.setDuration(1000);
+        v.startAnimation(animation);
         Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
         r.play();
@@ -26,6 +30,7 @@ public class emergency extends AppCompatActivity {
     public void callEmergency(View v){
         String phone = "8032615990";
         Intent intent = new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phone, null));
-        startActivity(intent);    }
+        startActivity(intent);
+    }
 
 }
